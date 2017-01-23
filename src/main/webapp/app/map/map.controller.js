0224b2c5ecb8bb1018de9836965353681b8d3ff6
Tom <tomyitav@gmail.com>
2017-01-23 21:21:52 +0200

    Small change in cesium map

--- src/main/webapp/app/map/map.controller.js
+++ src/main/webapp/app/map/map.controller.js
@@ -10,21 +10,22 @@
     function MapController (Principal, Auth, JhiLanguageService, $translate, $http) {
         var vm = this;
 
-        var west = 122.0;
-        var south = 33.0;
-        var east = 126.0;
-        var north = 40.0;
-        var rectangle = Cesium.Rectangle.fromDegrees(west, south, east, north);
-        Cesium.Camera.DEFAULT_VIEW_FACTOR = 0;
-        Cesium.Camera.DEFAULT_VIEW_RECTANGLE = rectangle;
+        function initializeCesium() {
+            var west = 32.0;
+            var south = 27.0;
+            var east = 34.0;
+            var north = 35.0;
+            var rectangle = Cesium.Rectangle.fromDegrees(west, south, east, north);
+            Cesium.Camera.DEFAULT_VIEW_FACTOR = 0;
+            Cesium.Camera.DEFAULT_VIEW_RECTANGLE = rectangle;
 
-        vm.viewer = new Cesium.Viewer('cesiumContainer', {
-            baseLayerPicker: false,
-            fullscreenButton: false,
-            vrButton: false,
-            infoBox: false,
-            timeline: false,
-            animation: false
-        });
+            vm.viewer = new Cesium.Viewer('cesiumContainer', {
+                fullscreenButton: false,
+                timeline: false,
+                animation: false
+            });
+        }
+
+        initializeCesium();
     }
 })();
