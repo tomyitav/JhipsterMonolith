72a8a3ed418bfb5648c43266935f3fd33af4e96a
Tom <tomyitav@gmail.com>
2017-01-23 21:05:04 +0200

    Fixed double map "problem"

--- src/main/webapp/app/map/map.controller.js
+++ src/main/webapp/app/map/map.controller.js
@@ -9,6 +9,22 @@
 
     function MapController (Principal, Auth, JhiLanguageService, $translate, $http) {
         var vm = this;
-        vm.viewer = new Cesium.Viewer('cesiumContainer');
+
+        var west = 122.0;
+        var south = 33.0;
+        var east = 126.0;
+        var north = 40.0;
+        var rectangle = Cesium.Rectangle.fromDegrees(west, south, east, north);
+        Cesium.Camera.DEFAULT_VIEW_FACTOR = 0;
+        Cesium.Camera.DEFAULT_VIEW_RECTANGLE = rectangle;
+
+        vm.viewer = new Cesium.Viewer('cesiumContainer', {
+            baseLayerPicker: false,
+            fullscreenButton: false,
+            vrButton: false,
+            infoBox: false,
+            timeline: false,
+            animation: false
+        });
     }
 })();
